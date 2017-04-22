15f756890304569b39453bf9f86f3bc5f2e78228
Basile Henry <bjm.henry@gmail.com>
2017-04-22 17:18:24 +0100

    SQL database

---
+++ create_tables.sql
@@ -0,0 +1,26 @@
+-- DROP Tables (For testing purposes)
+DROP TABLE USERS, MESSAGES CASCADE;
+
+CREATE TABLE USERS (
+    id              serial,
+    name            text,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE MESSAGES (
+    id               serial,
+    content_type     text,
+    content          text,
+    user_id          int,
+    creation_time    bigint,
+    expiry_time      bigint,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES USERS(id)
+);
+
+--  INSERT INTO LIBRARIES VALUES
+--  (0, 'Test Library', '127.0.0.1', 20, '55.947311, -3.201912');
+
+--  INSERT INTO USERS VALUES
+--  (DEFAULT, 'Basile', 'token'),
+--  (DEFAULT, 'Jack', 'token'),
