85ed801c557dc78d85c98cf327667a502ccc0ab8
Basile Henry <basile@teal.basilehenry.com>
2017-04-23 07:56:45 +0000

    fghjklhhusdfg

--- create_tables.sql
+++ create_tables.sql
@@ -14,3 +14,6 @@ CREATE TABLE MESSAGES (
     lng              float,
     PRIMARY KEY (id)
 );
+
+GRANT ALL PRIVILEGES ON TABLE messages TO copunhackers;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO copunhackers;
